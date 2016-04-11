35ea7174b8308dde5d7a993e4170eb0961a0467e
Brandon Hafenrichter <bhafenri@gmail.com>
2016-04-10 17:12:43 -0700

    Initial Commit
    This code contains all of the boiler plate code necessary to run ASP.Net
    MVC apps.  I have also set up a database and generated an empty model.
    This is to get any of the extra overhead out of the way early so I can
    work on the code itself.  The following is the requirements I received
    from Vaco.
    
    Project Description
    
    1 Hour Time Limit
    1.       Develop a website that handles the download of a file.
    2.       Use ASP.NET, MVC 5, Bootstrap, database, etc.
    3.       Collect some data from user before they can download (like,
    name, email address, physical address, etc.)
    4.       User won't be able to share the download link.
    5.       Result should be submitted via dropbox link.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BH_FileShare.MvcApplication" Language="C#" %>
---
+++ Services/HomeService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BH_FileShare.Services
+{
+    public class HomeService
+    {
+
+    }
+}
